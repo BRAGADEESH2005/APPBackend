af2c59dbeadacc2a2a00db54bb99f2706c471a38
BRAGADEESH V <bragadeesh2005@gmail.com>
2025-03-22 14:43:24 +0530

    first commit

---
+++ src/constants/index.ts
@@ -0,0 +1,7 @@
+const commonLanguages = [
+  { id: 50, name: 'C' },
+  { id: 54, name: 'C++' },
+  { id: 91, name: 'Java' },
+  { id: 92, name: 'Python' },
+];
+export { commonLanguages };
---
+++ src/interfaces/config.interface.ts
@@ -0,0 +1,48 @@
+interface ApiConfigProps {
+  apiUrl: string;
+  httpTimeout: number;
+}
+
+interface MongodbConfigProps {
+  connectionString: string;
+  databaseName: string;
+}
+
+export interface ConfigProps {
+  port: number;
+  api: ApiConfigProps;
+  mongodb: {
+    database: MongodbConfigProps;
+  };
+}
+export interface signup {
+  access_token: string;
+  message: string;
+}
+
+export interface codesnipet {
+  lang_id: number;
+  code: string;
+}
+
+export interface metadata {
+  input_format: string;
+  output_format: string;
+  judge_input_temple: string;
+  variable_names: Record<string, string>;
+  variable_types: Record<string, string>;
+}
+
+export interface submission {
+  problemId: string;
+  submissionId: string;
+  languageId: number;
+  status: string;
+  submittedAt: Date;
+}
+export enum supportedlanguages {
+  'C' = 50,
+  'C++' = 54,
+  'Java' = 91,
+  'Python' = 92,
+}
